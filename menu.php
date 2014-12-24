372d19caf20aa9bf474491692f9fe98385975fe8
alimasyhur <jegrag4ever@gmail.com>
2014-12-25 06:02:55 +0700

    Commit kedua

---
+++ menu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<ul class="nav nav-tabs alert-danger" role="tablist" style="background-color: black;">
+    <li><a href="index.php" style="color: whitesmoke;">Points and Lines</a></li>
+    <li><a href="transformation.php" style="color: whitesmoke">Rectangle Transformation</a></li>
+    <li><a href="triangletrans.php" style="color: whitesmoke">Triangle Transformation</a></li>
+    <li><a href="circle.php" style="color: whitesmoke">Circle</a></li>
+	<li><a href="clipping.php" style="color: whitesmoke">Dot Clipping</a></li>
+	<li><a href="lineclipping.php" style="color: whitesmoke">Line Clipping</a></li>
+    <li><a href="clipRect.php" style="color: whitesmoke">Polygon</a></li>
+    <li><a href="rotate3d.php" style="color: whitesmoke;">3D Rotate</a></li>
+    <li><a href="about_us.php" style="color: whitesmoke;">About Us</a></li>
+    
+
+</ul>
